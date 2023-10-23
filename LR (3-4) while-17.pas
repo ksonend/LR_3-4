36aee64eabaa7bfe264a5deb1b54c1302dafc765
ksonend <144596502+ksonend@users.noreply.github.com>
2023-10-23 23:44:19 +0300

    Add files via upload

---
+++ LR (3-4) while-17.pas
@@ -0,0 +1,10 @@
+﻿var q,n,z: integer;
+begin
+  readln(n);
+ while q <> n do
+    begin
+    z:= z + 2;
+    q:= q + 1;
+    end;
+  writeln(z,' бактерий за ',n,' минут.');
+end.
---
+++ LR (3-4) while-4.pas
@@ -0,0 +1,10 @@
+﻿var i,q: integer;
+begin
+i:=4;           //for i := 4 to 37 do
+while i<>38 do
+  begin
+    q:= q + sqr(i);
+    i:=i+1;
+  end;
+  writeln (q);
+end.
