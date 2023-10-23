9f0dfc4b29abfc95cb48d4abbb4025d8ad1163de
ksonend <144596502+ksonend@users.noreply.github.com>
2023-10-23 22:28:49 +0300

    Add files via upload

---
+++ LR(3-4)_repeat-13.pas
@@ -0,0 +1,14 @@
+﻿var s,q,w:integer;
+v: real;
+Begin
+  q:=3;
+  repeat
+    w:= w + q;
+    write (q:3);
+    q:=q+6;         
+    s:=s +1;
+  until s = 10 ;
+  v:= w / 10;
+ writeln ();
+ writeln ('ответ = ',v);
+End.
