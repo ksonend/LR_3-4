af163d369bdd1adb1ec598559ebb9a26eb38d8aa
ksonend <144596502+ksonend@users.noreply.github.com>
2023-10-24 00:22:44 +0300

    Add files via upload

---
+++ LR (3-4) repeat-7.pas
@@ -0,0 +1,12 @@
+﻿var n,z,x,y: real;
+begin
+  write ('отрезок: ');
+  read (x,y);
+  write ('шаг: ');
+  read (n);  
+ repeat
+    z:= 3*sqr(x) - exp(ln(2)*x);
+    x:= x + n;
+    writeln (' z:',z:5:2);
+ until x > y ; 
+end.
