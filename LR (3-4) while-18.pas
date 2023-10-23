0200da8865c95b956e8d55e442ae1dc81859d4f7
ksonend <144596502+ksonend@users.noreply.github.com>
2023-10-23 22:25:19 +0300

    Add files via upload

---
+++ LR (3-4) while-18.pas
@@ -0,0 +1,12 @@
+﻿var q, x, y: integer;
+ z: real;
+begin
+  readln(x,y);
+  q:= 1;
+ while q <>11 do
+    begin
+    z:= z + (q * exp(ln(x * y) * 1/q));
+    q:= q + 1;
+    end;
+  writeln('Ответ: ',z:2:2);
+end.
